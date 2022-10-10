33b699d5cffc7ca5830fa23c07f97ffec67c8bfd
zaczig <beastzac@yahoo.com>
2022-10-10 04:15:11 -0500

    added specific customization based on menu item

--- guiwork/serverCustomize.java
+++ guiwork/serverCustomize.java
@@ -2,35 +2,66 @@ import java.awt.FlowLayout;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 // THIS IS THE NEW WINDOW THAT OPENS TO CUSTOMIZE AN ITEM
 // WILL USE CHECKBOXES FOR INGREDIENTS
+// ONLY SOME ITEMS NEED TO HAVE CUSTOMIZATION
 public class serverCustomize {
-  JLabel bacon;
   serverCustomize(Integer i){
     Integer food_id = i + 1; //Match to database number
 
     // FRAME SETUP AND CONFIG
     JFrame frame = new JFrame();
-    frame.setSize(500,500);
+    frame.setSize(200,200);
     frame.setLayout(new FlowLayout());
     frame.setResizable(false);
 
-    // if its a burger
-    if(food_id < 6){
-      JCheckBox cheese = new JCheckBox("CHEESE");
-      cheese.setSelected(true);
-      JCheckBox lettuce = new JCheckBox("LETTUCE");
-      lettuce.setSelected(true);
-      JCheckBox bacon = new JCheckBox("BACON");
-      bacon.setSelected(true);
-
-      frame.add(lettuce);
-      frame.add(cheese);
-      frame.add(bacon);
+    // Create checkboxes for all possible ingredients
+    JCheckBox ingredients[] = new JCheckBox[8];
+    ingredients[0] = new JCheckBox("LETTUCE");
+    ingredients[1] = new JCheckBox("TOMATO");
+    ingredients[2] = new JCheckBox("ONION");
+    ingredients[3] = new JCheckBox("PICKLES");
+    ingredients[4] = new JCheckBox("BACON");
+    ingredients[5] = new JCheckBox("GIG EM SAUCE");
+    ingredients[6] = new JCheckBox("AMERICAN CHEESE");
+    ingredients[7] = new JCheckBox("GRAVY");
+    for(Integer j = 0; j < 8; j++){
+      // Sets all checkboxes to selected by default
+      ingredients[j].setSelected(true); 
     }
 
+    if(food_id == 1){
+      frame.add(ingredients[4]);
+      frame.add(ingredients[6]);
+    }
+
+    if(food_id == 2 || food_id == 5 || food_id == 6){
+      frame.add(ingredients[0]);
+      frame.add(ingredients[1]);
+      frame.add(ingredients[3]);
+      frame.add(ingredients[2]);
+    }
+
+    if(food_id == 4){
+      frame.add(ingredients[6]);
+      frame.add(ingredients[1]);
+      frame.add(ingredients[3]);
+      frame.add(ingredients[2]);
+    }
+
+    if(food_id == 8){
+      frame.add(ingredients[6]);
+      frame.add(ingredients[2]);
+      frame.add(ingredients[5]);
+    }
+
+    if(food_id == 9){
+      frame.add(ingredients[6]);
+      frame.add(ingredients[2]);
+      frame.add(ingredients[1]);
+      frame.add(ingredients[0]);
+    }
 
     frame.setVisible(true);
   }
