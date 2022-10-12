21f2dadf31d0dabdc5a64aa7f27a855352018821
Nick Knighton <nickk@tamu.edu>
2022-10-11 19:57:12 -0500

    added layout for manager GUI

--- guiwork/managerMain.java
+++ guiwork/managerMain.java
@@ -1,102 +1,62 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
 
-public class managerMain implements ActionListener{
-  String names[] = {"Bacon Cheeseburger", "Black Bean Burger", "BYO Burger", "Cheeseburger", "Double Stack Cheeseburger", "Classic Hamburger", "Chicken Caesar Salad", "Gig Em Patty Melt", "Grilled Chicken Sandwich", "Double Scoop Ice Cream", "Aggie Shake", "Cookie Ice Cream Sundae", "French Fries", "Tater Tots", "Onion Rings", "Kettle Chips", "Aquafina 16 oz", "Aquafina 20 oz", "Fountain Drink", "Chicken Tender Basket"};
-
-  Double prices[] = {7.89,7.29,6.49,6.99,9.69,6.49,8.29,7.09,7.49,3.29,4.49,4.49,2.69,2.69,2.69,2.69,1.79,2.19,2.45,6.79,7.29};
-
-  JButton menuButtons[] = new JButton[20];
-  Double runTot = 0.0; //Total price of order that is displayed to total side of screen
-  DecimalFormat df = new DecimalFormat("0.00");
+public class managerMain{
   managerMain() {
     // DEFINING MAIN J OBJECTS USED
-    JFrame frame = new JFrame(); 
-    JPanel menu = new JPanel(); //FIRST TAB
-    JPanel menu2 = new JPanel(); //SECOND TAB
-    JPanel total = new JPanel(); //RIGHT SIDE OF GUI WHERE RUNNING TOTAL IS KEPT
-    JTabbedPane tabbedPane = new JTabbedPane(); //ALLOWS FOR TABBED MENU
-    JLabel totalTitle = new JLabel("Order Total");
-    Color buttonColor = new Color(0xEF3054);
     Color primary = new Color(0x2A2A72);
     Font guiFont = new Font("Impact",Font.PLAIN,20);
 
-    // CONFIG AND LAYOUT
+    //Create frame
+    JFrame frame = new JFrame();  
+
+    //Create critically low pane
+    JPanel criticallyLow = new JPanel();
+    JLabel criticallyLowTitle = new JLabel("Critically Low Inventory");
+    
+    //Create item range page
+    JPanel itemRange = new JPanel();
+    JLabel itemRangeTitle = new JLabel("Item History");
+    
+    //Create current inventory
+    JPanel currentInventory = new JPanel();
+    JLabel currentInventoryTitle = new JLabel("Current Inventory");
+    
+    //Sets up the frame
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(1000,1000);
     frame.setLayout(null);
     frame.setResizable(false);
-
-    tabbedPane.setBounds(0,0,500,1000);
-
-    total.setBounds(500,0,500,1000);
-    total.setBackground(primary);
-    totalTitle.setForeground(Color.white);
-    totalTitle.setFont(guiFont);
-
-    menu.setLayout(new GridLayout(5,4,10,10)); // args is rows, columns
-    menu2.setLayout(new GridLayout(5,4,10,10)); // args is rows, columns
-
-    // POPULATING BOTH MENUS WITH BUTTONS
-    Integer j = 10; //counter for second page (starts at index 10)
-    for(Integer i = 0; i < 10; i++){ // counter for first page (starts at index 0)
-      menuButtons[i] = new JButton(names[i]);
-      menuButtons[j] = new JButton(names[j]);
-      menuButtons[i].setBackground(buttonColor);
-      menuButtons[i].setFont(guiFont);
-      menuButtons[i].addActionListener(this);
-
-      menuButtons[j].setBackground(buttonColor);
-      menuButtons[j].setFont(guiFont);
-      menuButtons[j].addActionListener(this);
-      menu.add(menuButtons[i]);
-      menu2.add(menuButtons[j]);
-
-      j++;
-    }
-
-    // POPULATING TOTAL SIDE
-    JButton checkout = new JButton("CHECKOUT");
-    checkout.setBounds(100,0,300,50);
-    checkout.setBackground(buttonColor);
-    // checkout.setHorizontalAlignment(JButton.RIGHT);
-    // checkout.setVerticalAlignment(JButton.BOTTOM);
-
-
-    // ADDING ITEMS TO PROPER CONTAINER
-    tabbedPane.add("Menu1", menu);
-    tabbedPane.add("Menu2", menu2);
-    total.add(totalTitle);
-    total.add(checkout);
-    frame.add(tabbedPane);
-    frame.add(total);
+    frame.add(criticallyLow);
+    frame.add(itemRange);
+    frame.add(currentInventory);
     frame.setVisible(true);
-  }
+    
+    //Sets up critically low
+    criticallyLow.setBounds(0,0,500,500);
+    criticallyLow.setBackground(primary);
+    criticallyLowTitle.setForeground(Color.white);
+    criticallyLowTitle.setFont(guiFont);
+    criticallyLow.add(criticallyLowTitle);
 
-  // This will be used to open new window for customizations
-  @Override
-  public void actionPerformed(ActionEvent e) {
-
-      for(Integer i = 0; i < 20; i++){
-        if(e.getSource()==menuButtons[i]){
-          //the i variable will also be passed into the constructor
-          // this will allow for a specialized customize screen depending on the menu item
-          //runTot += names[i] + "  " + prices[i] + "\n";
-          System.out.println(names[i] + "  " + prices[i]);
-          runTot += prices[i];
-          new serverCustomize(i);
-        }
-      }
-      System.out.println("Total Price: " + df.format(runTot));
+    //Sets up item range
+    itemRange.setBounds(505,0,500,500);
+    itemRange.setBackground(primary);
+    itemRangeTitle.setForeground(Color.white);
+    itemRangeTitle.setFont(guiFont);
+    itemRange.add(itemRangeTitle);
+    
+    //Sets up current Inventory
+    currentInventory.setBounds(0,505,1000,500);
+    currentInventory.setBackground(primary);
+    currentInventoryTitle.setForeground(Color.white);
+    currentInventoryTitle.setFont(guiFont);
+    currentInventory.add(currentInventoryTitle);
   }
 }
