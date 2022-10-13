a112b243e514a746f06436b8981f8dc7f81e557c
zaczig <beastzac@yahoo.com>
2022-10-12 19:23:19 -0500

    Added feature to print current order and price

--- src/serverMain.java
+++ src/serverMain.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -7,6 +8,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
@@ -25,6 +28,9 @@ public class serverMain implements ActionListener{
   DecimalFormat df = new DecimalFormat("0.00");
   JButton checkout = new JButton("CHECKOUT");
   ArrayList<Integer> currOrder; //This will be the array for the items within the current order
+  JTextArea ongoingOrder = new JTextArea("   Current Order:\n", 35,35);
+  String order = "";
+  JLabel totalTitle = new JLabel("Order Total:     " + runTot, JLabel.CENTER);
   // Database object to communicate with the server
   Database db;
 
@@ -34,8 +40,9 @@ public class serverMain implements ActionListener{
     JPanel menu = new JPanel(); //FIRST TAB
     JPanel menu2 = new JPanel(); //SECOND TAB
     JPanel total = new JPanel(); //RIGHT SIDE OF GUI WHERE RUNNING TOTAL IS KEPT
+
     JTabbedPane tabbedPane = new JTabbedPane(); //ALLOWS FOR TABBED MENU
-    JLabel totalTitle = new JLabel("Order Total");
+
     Color buttonColor = new Color(0xEF3054);
     Color primary = new Color(0x2A2A72);
     Font guiFont = new Font("Impact",Font.PLAIN,20);
@@ -53,7 +60,11 @@ public class serverMain implements ActionListener{
     total.setBounds(500,0,500,1000);
     total.setBackground(primary);
     totalTitle.setForeground(Color.white);
+    totalTitle.setPreferredSize(new Dimension(250,100));
     totalTitle.setFont(guiFont);
+    totalTitle.setVerticalAlignment(JLabel.BOTTOM);
+    ongoingOrder.setBounds(600,300,400,1000);
+    ongoingOrder.setFont(new Font("Impact",Font.PLAIN,17));
 
     menu.setLayout(new GridLayout(5,4,10,10)); // args is rows, columns
     menu2.setLayout(new GridLayout(5,4,10,10)); // args is rows, columns
@@ -64,8 +75,7 @@ public class serverMain implements ActionListener{
     Integer j = 10; //counter for second page (starts at index 10)
     for(Integer i = 0; i < 10; i++){ // counter for first page (starts at index 0)
       try{
-        //menuButtons[i] = new JButton(names[i]);
-        //menuButtons[j] = new JButton(names[j]);
+
         menuItems.absolute(i+1);
         String name = menuItems.getString("menuitem");
         menuButtons[i] = new JButton(name);
@@ -90,18 +100,16 @@ public class serverMain implements ActionListener{
     }
 
     // POPULATING TOTAL SIDE
-
     checkout.addActionListener(this);
-    checkout.setBounds(100,0,300,50);
     checkout.setBackground(buttonColor);
-    // checkout.setHorizontalAlignment(JButton.RIGHT);
-    // checkout.setVerticalAlignment(JButton.BOTTOM);
+
 
 
     // ADDING ITEMS TO PROPER CONTAINER
     tabbedPane.add("Menu1", menu);
     tabbedPane.add("Menu2", menu2);
-    total.add(totalTitle);
+    total.add(totalTitle); //The order that these are added matters
+    total.add(ongoingOrder);
     total.add(checkout);
     frame.add(tabbedPane);
     frame.add(total);
@@ -112,6 +120,7 @@ public class serverMain implements ActionListener{
   @Override
   public void actionPerformed(ActionEvent e) {
     ResultSet menuItems;
+
       for(Integer i = 0; i < 20; i++){
         if(e.getSource()==menuButtons[i]){
           menuItems = db.executeQuery("SELECT * FROM menu ORDER BY food_id");
@@ -125,9 +134,12 @@ public class serverMain implements ActionListener{
             double price = menuItems.getDouble("price");
             int id = menuItems.getInt("food_id");
             currOrder.add(id);
-
-            System.out.println("Added to order: " + name + "  " + price);
+            order += "   " + name + "   $" + price + "\n";
+            System.out.println("Added to order: " + name + "   $" + price);
             runTot += price;
+            totalTitle.setText("Order Total:     $" + df.format(runTot));
+            ongoingOrder.setText(order);
+
             new serverCustomize(i);
           }catch(Exception ex){
             System.out.println(ex.getMessage());
